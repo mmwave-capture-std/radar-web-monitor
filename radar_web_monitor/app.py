c78b0f9edb3a0809259cfb8222d0bda934043383
Louie Lu <git@louie.lu>
2024-02-01 14:00:53 -0500

    Update to socket-io for real-time output

--- radar_web_monitor/app.py
+++ radar_web_monitor/app.py
@@ -2,8 +2,11 @@ import subprocess
 import shlex
 
 from flask import Flask, render_template, request, jsonify, Response
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "oaiesnt#!ehA21$&ndhienasrUns"
+socketio = SocketIO(app)
 
 
 @app.route("/")
@@ -11,23 +14,25 @@ def index():
     return render_template("index.html")
 
 
-def execute_command(command):
+def background_thread(command):
+    """Run a command and emit stdout lines to a WebSocket."""
     process = subprocess.Popen(
         shlex.split(command),
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True,
+        bufsize=1,
     )
     for line in iter(process.stdout.readline, ""):
-        yield line
+        socketio.emit("command_output", {"data": line})
     process.stdout.close()
     process.wait()
 
 
-@app.route("/execute", methods=["POST"])
-def execute():
-    command = request.form["command"]
-    return Response(execute_command(command), mimetype="text/plain")
+@socketio.on("execute_command")
+def handle_execute_command(json):
+    command = json["command"]
+    socketio.start_background_task(background_thread, command)
 
 
 if __name__ == "__main__":
