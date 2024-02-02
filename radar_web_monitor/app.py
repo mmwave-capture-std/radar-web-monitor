d551445434b3f3ae0eb165b8c8157002dd44d8c4
Louie Lu <git@louie.lu>
2024-02-02 16:05:46 -0500

    Add tab UI

--- radar_web_monitor/app.py
+++ radar_web_monitor/app.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import shlex
 
@@ -14,7 +15,7 @@ def index():
     return render_template("index.html")
 
 
-def background_thread(command):
+def background_thread(command, env):
     """Run a command and emit stdout lines to a WebSocket."""
     process = subprocess.Popen(
         shlex.split(command),
@@ -22,8 +23,12 @@ def background_thread(command):
         stderr=subprocess.STDOUT,
         text=True,
         bufsize=1,
+        env=env,
     )
-    for line in iter(process.stdout.readline, ""):
+    while True:
+        line = process.stdout.readline()
+        if not line:
+            break
         socketio.emit("command_output", {"data": line})
     process.stdout.close()
     process.wait()
@@ -32,8 +37,11 @@ def background_thread(command):
 @socketio.on("execute_command")
 def handle_execute_command(json):
     command = json["command"]
-    socketio.start_background_task(background_thread, command)
+    env = os.environ.copy()
+    env["PYTHONUNBUFFERED"] = "x"
+    socketio.start_background_task(background_thread, command, env=env)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
+    # app.run(debug=True)
